05b35de76fa3347bdb9f42c0a0bdcce5e0e48410
DL23727272 <113783762+DL23727272@users.noreply.github.com>
2024-05-31 00:02:11 +0800

    functional signup and login

---
+++ loginProcess.php
@@ -0,0 +1,61 @@
+<?php
+include "myConnection.php";
+
+    if (isset($_POST['studentLoginName']) && isset($_POST['studentLoginPassword'])) {
+        $username = $_POST["studentLoginName"];
+        $password = $_POST["studentLoginPassword"];
+
+
+        if ($username == "" || $password == "") {
+            $response = [
+                'status' => 'error',
+                'message' => 'Empty fields! Please fill all the fields.'
+            ];
+        } else {
+
+            $hashedPassword = md5($password);
+
+            $query = "SELECT * FROM users WHERE username = '$username'";
+            $result = mysqli_query($con, $query);
+
+            if (mysqli_num_rows($result) == 1) {
+                $row = mysqli_fetch_assoc($result);
+                // Verify hashed password
+                if ($hashedPassword === $row['password']) {
+                    $studentID = $row['id'];
+                    $studentName = $row['fullName'];
+                    $userType = $row['type'];
+                    // Passwords match, login successful
+                    $response = [
+                        'status' => 'success',
+                        'message' => 'Welcome!',
+
+                        'customerID' => $studentID,
+                        'customerName' => $studentName,
+                        'userType' => $userType
+
+                    ];
+                } else {
+                    $response = [
+                        'status' => 'error',
+                        'message' => 'Invalid username or password.'
+                    ];
+                }
+            } else {
+                // User doesn't exist
+                $response = [
+                    'status' => 'error',
+                    'message' => 'Invalid username or password.'
+                ];
+            }
+        }
+    } else {
+        $response = [
+            'status' => 'error',
+            'message' => 'All fields are mandatory'
+        ];
+    }
+
+    header('Content-Type: application/json');
+    echo json_encode($response);
+?>
